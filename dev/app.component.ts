109dee3d298a022f4147bbe10fd880be145008c0
Gebruiker <Gebruiker>
2016-04-10 23:10:20 +0200

    Twitter Bootstrap toegevoegd aan de boilerplate

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -3,7 +3,15 @@ import { Component } from 'angular2/core';
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Boilerplate app</h1>
+        <div class="col-md-12">
+            <h1>Boilerplate app</h1>
+        </div> 
+        <div class="col-md-6">
+            <span>Links</span>
+        </div>
+        <div class="col-md-6">
+            <span>Rechts</span>
+        </div>     
     `
 })
 

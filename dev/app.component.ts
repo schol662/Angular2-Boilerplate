a8c8c5252fc4786fa1479ddbd236c7dcdc2cdbc1
Jarno Scholten <jarno.scholten@tui.nl>
2016-04-11 09:21:21 +0200

    Aparte template file aangemaakt voor root component.

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -2,17 +2,7 @@ import { Component } from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: `
-        <div class="col-md-12">
-            <h1>Boilerplate app</h1>
-        </div> 
-        <div class="col-md-6">
-            <span>Links</span>
-        </div>
-        <div class="col-md-6">
-            <span>Rechts</span>
-        </div>     
-    `
+    templateUrl: '/dev/app.component.html'
 })
 
 export class AppComponent { }
